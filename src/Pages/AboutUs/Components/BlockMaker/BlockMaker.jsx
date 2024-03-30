3729d0519bebb587e504b5ef5545a536e0b95fec
SergeyBaturo <baturo.sergey77@gmail.com>
2024-03-30 14:22:52 +0300

    added images on 'Home' & 'About Us' pages

--- src/Pages/AboutUs/Components/BlockMaker/BlockMaker.jsx
+++ src/Pages/AboutUs/Components/BlockMaker/BlockMaker.jsx
@@ -1,9 +1,9 @@
 import './BlockMaker.css'
 
-export default function BlockMaker() {
+export default function BlockMaker(i) {
     return (
         <div className='block'>
-            <img src="" alt="someImage" />
+            <img src={i.i} alt="someImage" />
             <h1>Some title</h1>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati placeat laborum inventore exercitationem quod. Consectetur?</p>
             <button>Читать дальше</button>
