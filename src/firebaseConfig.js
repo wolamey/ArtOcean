9fa2599d70629c3374bc96d92dbd980c81597e70
wolamey <salamenikava77@gmail.com>
2024-05-15 15:47:43 +0300

    omg

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,6 +1,7 @@
 export default {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGIN_SENDER_ID,
