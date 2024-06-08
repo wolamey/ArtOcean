b33f380ab0d6fbda3f07b2ba77fc77c5b65c15bd
wolamey <salamenikava77@gmail.com>
2024-06-08 14:23:19 +0300

    main-page corrected and translated

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -16,119 +16,123 @@ function App() {
             <Carousel.Item>
               <div className="containerCarousel containerCarousel1">
                 <div className="homeTextBlock">
-                  <h1>Хит продаж</h1>
+                  <h1>Best seller</h1>
                   <h3 className="h3">Dress “Moon Inspiration”</h3>
                   <div className="price">
-                    <h3>Цена : </h3>
+                    <h3>Price : </h3>
                     <p>150$</p>
                   </div>
                 </div>
+                <img className="carousel__img" src="./../../../public/home1.png" alt="" />
               </div>
             </Carousel.Item>
             <Carousel.Item>
-              <div className="containerCarousel containerCarousel2">
+              <div className="containerCarousel containerCarousel1">
                 <div className="homeTextBlock">
-                  <h1>Лучшее из новинок</h1>
-                  <h3 className="h3">Macramé weave “Evening Tranquility”</h3>
+                  <h1>Best seller</h1>
+                  <h3 className="h3">Theater mask “The Face of the Soul”</h3>
                   <div className="price">
-                    <h3>Цена : </h3>
+                    <h3>Price : </h3>
                     <p>150$</p>
                   </div>
                 </div>
+                <img className="carousel__img" src="./../../../public/home2.png" alt="" />
+
               </div>
             </Carousel.Item>
             <Carousel.Item>
-              <div className="containerCarousel containerCarousel3">
+              <div className="containerCarousel containerCarousel1">
                 <div className="homeTextBlock">
-                  <h1>Хит продаж</h1>
-                  <h3 className="h3">Embroidered shawl “Oriental motifs”</h3>
+                  <h1>Best seller</h1>
+                  <h3 className="h3">Sculpture “Harmony of Movement”</h3>
                   <div className="price">
-                    <h3>Цена : </h3>
+                    <h3>Price : </h3>
                     <p>70$</p>
                   </div>
                 </div>
+                <img className="carousel__img" src="./../../../public/home3.png" alt="" />
+
               </div>
             </Carousel.Item>
           </Carousel>
         </div>
         <div className="bestSells">
           <div className="container">
-            <h1>Лучшие продажи</h1>
+            <h1>Best Sales</h1>
           </div>
           <DarkCarousel />
           <div className="container">
-            <NavLink to="catalog" className="katalogButton">Перейти в каталог</NavLink>
+            <NavLink to="catalog" className="katalogButton">Go to the catalog</NavLink>
           </div>
         </div>
         <div className="newItems">
           <div className="container">
-            <h1>Новинки</h1>
+            <h1>New products</h1>
           </div>
           <DarkCarousel />
           <div className="container">
-            <NavLink to="catalog" className="katalogButton">Перейти в каталог</NavLink>
+            <NavLink to="catalog" className="katalogButton">Go to the catalog</NavLink>
           </div>
         </div>
         <div className="littleBaner">
           <div className="container">
             <div className="littleBanerTextContent">
               <div className="number">1 + 1 = 3</div>
-              <p>Закажите два товара и получите третий в подарок</p>
-              <NavLink to="catalog" className="katalogButton">Перейти в каталог</NavLink>
+              <p>Order two items and get the third one as a gift</p>
+              <NavLink to="catalog" className="katalogButton">Go to the catalog</NavLink>
             </div>
           </div>
         </div>
         <div className="whyPeopleChooseUs">
           <div className="container">
-            <h1>Почему выбирают нас</h1>
+            <h1>Why choose us</h1>
             <div className="blocks">
               <div className="block">
                 <img src="/authenticity.jpg" />
-                <h1>Лучшие</h1>
-                <p>Наш сай является лучшим в этой сфере</p>
+                <h1>The best</h1>
+                <p>Our sai is the best in this field</p>
               </div>
               <div className="block">
                 <img src="/sophistication.jpg" />
-                <h1>изысканность</h1>
-                <p>изысканность - наш приоретет</p>
+                <h1>Sophistication</h1>
+                <p>Sophistication is our priority</p>
               </div>
               <div className="block">
                 <img src="/fairPayment.jpg" />
-                <h1>справедливая оплата</h1>
-                <p>Все товары стоят справедливую цену</p>
+                <h1>fair payment</h1>
+                <p>All products are worth a fair price</p>
               </div>
               <div className="block">
                 <img src="/bigAsortiment.jpg" />
-                <h1>большой асортимент</h1>
+                <h1>large assortment</h1>
                 <p>
-                  У нас самый большой выбор товаров среди подобных магазинов
+                We have the largest selection of products among similar stores
                 </p>
               </div>
               <div className="block">
                 <img src="/deliviryAllOverTheWorld.jpg" />
-                <h1>Доставка по всему миру</h1>
-                <p>Мы предостовляем недорогую доставку по всему миру</p>
+                <h1>Worldwide shipping</h1>
+                <p>We provide low-cost worldwide shipping</p>
               </div>
               <div className="block">
                 <img src="/qualityAssurance.jpg" />
-                <h1>гарантия качества</h1>
+                <h1>Quality assurance</h1>
                 <p>
-                  Мы гарантируем качество наших товаров и безопасную их
-                  транспортировку
+                We guarantee the quality of our products and their safety
+                transportation
                 </p>
               </div>
               <div className="block">
                 <img src="/comfortToUse.jpg" />
-                <h1>лёгкое использование</h1>
+                <h1>Easy to use</h1>
                 <p>
-                  Всё легко и понятно
-                </p>
+                Everything is easy and understandable                </p>
               </div>
               <div className="block">
                 <img src="/carryAboutNature.jpg" />
-                <h1>Забота о природе</h1>
+                <h1>Taking care of nature</h1>
                 <p>
-                  Все товары в магазине экологически чистые
+                All products in the store are environmentally friendly  
                 </p>
               </div>
             </div>
